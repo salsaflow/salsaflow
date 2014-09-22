6578a8354087316bd894edec3d8f04bc2ef95cde
Tomas Brambora <tomas.brambora@gmail.com>
2014-09-22 21:10:05 +0800

    Multiline strings in ASCII art.
    Change-Id: aaa1e85957

--- git-trunk/asciiart/ascii.go
+++ git-trunk/asciiart/ascii.go
@@ -6,24 +6,24 @@ import (
 
 func PrintThumbsUp() {
 	// from http://emilights.com/ascii-auto-text-art/expression/217-thumbs-up
-	log.Println("        __               __        ")
-	log.Println("       (  |             |  )       ")
-	log.Println("  _____ \\  \\           /  /_____   ")
-	log.Println(" (____ _)   \\         /   (_____)  ")
-	log.Println(" (_____ )  _)__(. .)__(_  ( _____) ")
-	log.Println(" (__ ___)   )  |___|  (   (_  ___) ")
-	log.Println("  (_____)__/   /_/\\_\\  \\__(____)   ")
+	log.Println(`        __               __
+       (  |             |  )
+  _____ \  \           /  /_____
+ (____ _)   \   ___   /   (_____)
+ (_____ )  _)__(. .)__(_  ( _____)
+ (__ ___)   )  |___|  (   (_  ___)
+  (_____)__/   /_/\_\  \__(____)`)
 }
 
 func PrintScream(header, msg string) {
 	log.Println(header)
-	log.Println("     .----------.   ")
-	log.Println("    /  .-.  .-.  \\  ")
-	log.Println("   /   | |  | |   \\ ")
-	log.Println("   \\   `-'  `-'  _/ ")
-	log.Println("   /\\     .--.  / | ")
-	log.Println("   \\ |   /  /  / /  ")
-	log.Println("   / |  `--'  /\\ \\  ")
-	log.Println("    /`-------'  \\ \\ ")
+	log.Println(`   .----------. 
+  /  .-.  .-.  \
+ /   | |  | |   \
+ \   ` + "`" + `-'  ` + "`" + `-'  _/
+ /\     .--.  / |
+ \ |   /  /  / /
+ / |  ` + "`" + `--'  /\ \
+  /` + "`" + `-------'  \ \`)
 	log.Println(msg)
 }
