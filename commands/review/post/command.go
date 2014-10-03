29e9965305bf0e38ba3e7d407d39583be5b4cc1a
Ondřej Kupka <ondra.cap@gmail.com>
2014-10-03 14:52:24 +0200

    review post: Make the output more consistent
    Review the review post subcommand and make the output a bit more consistent with
    other commands. Also dropped a few funky sentences from the output, it is more
    irritating than helping to see the important info. Sorry, I am boring, I know.
    
    Change-Id: bae17f3055

--- commands/review/post/command.go
+++ commands/review/post/command.go
@@ -1,18 +1,24 @@
 package postCmd
 
 import (
+	// Stdlib
 	"bytes"
 	"fmt"
+	"io"
 	"os"
+	"text/tabwriter"
 
+	// Internal
 	"github.com/salsita/salsaflow/app"
 	"github.com/salsita/salsaflow/asciiart"
 	"github.com/salsita/salsaflow/config"
+	"github.com/salsita/salsaflow/errors"
 	"github.com/salsita/salsaflow/git"
 	"github.com/salsita/salsaflow/log"
 	"github.com/salsita/salsaflow/prompt"
 	"github.com/salsita/salsaflow/shell"
 
+	// Other
 	"gopkg.in/tchap/gocli.v1"
 )
 
@@ -38,8 +44,13 @@ func run(cmd *gocli.Command, args []string) {
 	}
 }
 
+func handleError(task string, err error, stderr *bytes.Buffer) error {
+	errors.NewError(task, stderr, err).Log(log.V(log.Info))
+	return err
+}
+
 type readResult struct {
-	sha    string
+	msg    string
 	stdout *bytes.Buffer
 	stderr *bytes.Buffer
 	err    error
@@ -52,81 +63,97 @@ func runMain() (err error) {
 		stderr        *bytes.Buffer
 	)
 
+	// Remember the current branch.
+	msg := "Remember the current branch"
+	log.Run(msg)
 	currentBranch, stderr, err = git.CurrentBranch()
 	if err != nil {
-		log.FailWithDetails("get current branch", stderr)
-		return err
+		return handleError(msg, err, nil)
 	}
 
+	// Parse the branch name. Return in case we are not on a story branch.
+	msg = "Parse the branch name"
 	storyId, err = git.RefToStoryId(currentBranch)
 	if err != nil {
 		_, ok := err.(*git.ErrNotStoryBranch)
 		if !ok {
-			return err
+			return handleError(msg, err, nil)
 		}
-		log.Fail("No story branch detected.")
-		log.Println("I'm sorry Dave, I can't let you do that. You have to checkout a story branch.")
-		log.Fatalln("Have a good day!")
+		return handleError(msg, err, nil)
 	}
 
+	// Fetch the remote repository.
+	msg = "Fetch the remote repository"
+	log.Run(msg)
+	if stderr, err := git.UpdateRemotes(config.OriginName); err != nil {
+		return handleError(msg, err, stderr)
+	}
+
+	// Make sure the trunk branch is up to date.
+	msg = "Make sure the trunk branch is up to date"
+	log.Run(msg)
+	stderr, err = git.EnsureBranchSynchronized(config.TrunkBranch, config.OriginName)
+	if err != nil {
+		return handleError(msg, err, stderr)
+	}
+
+	// Get all the commits that are new compared to the trunk branch.
+	msg = "List and parse the commits on the story branch"
 	commits, stderr, err := git.ListBranchCommits(currentBranch, config.TrunkBranch)
 	if err != nil {
-		log.FailWithDetails("Listing commits on the story branch", stderr)
-		return err
+		return handleError(msg, err, stderr)
 	}
 
-	originTrunkBranch := fmt.Sprintf("%s/%s", config.OriginName, config.TrunkBranch)
-
-	log.Printf(`
+	// Tell the user what is going to happen and ask for confirmation.
+	msg = "Ask the user to confirm the actions to follow"
+	fmt.Printf(`
 You are posting reviews for story %s. Swell!
 
 Here's what's going to happen:
-	1) We'll rebase your branch '%s' on top of branch '%s'.
-	2) We'll post a review for each commit on your branch (that should be %d commits).`,
-		storyId, currentBranch, originTrunkBranch, len(commits))
+  1) Branch '%s' will be rebased onto branch '%s'.
+  2) A review request will be posted for each of the following commits:`,
+		storyId, currentBranch, config.TrunkBranch)
 
-	asciiart.PrintSnoopy()
-
-	log.Run(fmt.Sprintf("Fetching %s", config.OriginName))
-	if stderr, err := git.UpdateRemotes(config.OriginName); err != nil {
-		log.FailWithDetails(fmt.Sprintf("Could not fetch data from %s", config.OriginName), stderr)
-		return err
-	}
-
-	log.Run(fmt.Sprintf("Rebasing on top of %s", originTrunkBranch))
-	_, stderr, err = git.Git(
-		"rebase", fmt.Sprintf("%s", originTrunkBranch))
-	if err != nil {
-		log.FailWithDetails("Error when rebasing", stderr)
-		return err
-	}
-	log.Printf("Sweet, branch '%s' is now totally rebased on top of %s!\n\n",
-		currentBranch, originTrunkBranch)
-
-	log.Println("I will now post reviews for the following commits:")
+	tw := tabwriter.NewWriter(os.Stdout, 0, 8, 4, '\t', 0)
+	io.WriteString(tw, "\n\n")
+	io.WriteString(tw, "     Commit SHA\tCommit Title\n")
+	io.WriteString(tw, "     ==========\t============\n")
 	for _, commit := range commits {
-		log.Printf("  %s (%s)\n", commit.SHA, commit.Title)
+		fmt.Fprintf(tw, "     %v\t%v\n", commit.SHA, commit.Title)
 	}
-	log.Println()
+	io.WriteString(tw, "\n")
+	tw.Flush()
 
 	confirmed, err := prompt.Confirm("You cool with that?")
 	if err != nil {
-		return err
+		return handleError(msg, err, nil)
 	}
 	if !confirmed {
-		log.Println("I will exit now. If you want to try again, just run me again.")
-		log.Println("Have a nice day!")
+		fmt.Println("\nFair enough, have a nice day!")
 		return nil
 	}
 
+	asciiart.PrintSnoopy()
+
+	// Rebase the story branch on top of the trunk branch.
+	msg = fmt.Sprintf("Rebase the story branch on top of branch '%v'", config.TrunkBranch)
+	log.Run(msg)
+	_, stderr, err = git.Git("rebase", config.TrunkBranch)
+	if err != nil {
+		return handleError(msg, err, stderr)
+	}
+
+	// Post the review requests.
+	msg = "Post the review requests"
 	resCh := make(chan *readResult, len(commits))
 
 	for _, commit := range commits {
 		go func(commit *git.Commit) {
-			log.Go(fmt.Sprintf("Posting review for commit %s", commit.SHA))
-			out, stderr, err := shell.Run(
+			msg := "Post the review request for commit " + commit.SHA
+			log.Go(msg)
+			stdout, stderr, err := shell.Run(
 				"rbt", "post", "--guess-fields", "yes", "--branch", storyId, commit.SHA)
-			resCh <- &readResult{commit.SHA, out, stderr, err}
+			resCh <- &readResult{msg, stdout, stderr, err}
 		}(commit)
 	}
 	for i := 0; i < cap(resCh); i++ {
@@ -135,23 +162,26 @@ Here's what's going to happen:
 		}
 	}
 
-	log.Println()
+	// Tell the user what to do next.
 	log.Println(`
-Please take some time go through the reviews, check and annotate them for the reviewer.
+----------
 
-If you find any issues you want to fix right before publishing, fix them now, amend
-the relevant commits and use:
+Now, please, take some time to go through all the review requests,
+check and annotate them for the reviewers to make them more happy (less sad).
 
-rbt post -r <RB request id> --parent ` + config.TrunkBranch + ` <commit SHA>
+If you find any issues you want to fix right before publishing, fix them now,
+amend the relevant commits and use:
 
-to update the review.
+  $ rbt post -r <RB request id> <commit SHA>
+
+to update the relevant review request.
 
   ###########################################################
   # IMPORTANT: Your code has not been merged and/or pushed. #
   ###########################################################
 
-When you think the reviews are ready to be published, publish them in Review Board.
-Then merge your branch into ` + config.TrunkBranch + ` and push.
+When you think the review requests are ready to be published,
+publish them in Review Board. Then merge your branch into ` + config.TrunkBranch + ` and push.
 `)
 
 	return nil
@@ -159,14 +189,13 @@ Then merge your branch into ` + config.TrunkBranch + ` and push.
 
 func logRbtOutput(res *readResult) {
 	var logger = log.V(log.Info)
-	logger.Lock()
-	defer logger.Unlock()
 	if res.err != nil {
-		logger.UnsafeFail(
-			fmt.Sprintf("Could not post review request for commit %s", res.sha))
-		logger.UnsafeStderr(res.stderr)
+		errors.NewError(res.msg, res.stderr, res.err).Log(logger)
 	} else {
-		logger.UnsafeOk(fmt.Sprintf("Review for commit %s posted", res.sha))
+		logger.Lock()
+		logger.UnsafeNewLine("")
+		logger.UnsafeOk(res.msg)
 		logger.UnsafePrint(res.stdout)
+		logger.Unlock()
 	}
 }
