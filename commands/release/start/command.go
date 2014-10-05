7745960536e7e820bfaa20e6ec024e115c696174
Ondřej Kupka <ondra.cap@gmail.com>
2014-10-05 22:24:41 +0200

    release start: Review the command and fix errors
    Update release start command to the latest conventions and also fix some
    critical bugs found on the way, particularly somehow completely wrong order of
    steps being executed. For example, the version string was first read from the
    trunk branch, then the repository was updated from the remote. Exactly the
    opposite to what it should be.
    
    Change-Id: 2ea30ab39f

--- commands/release/start/command.go
+++ commands/release/start/command.go
@@ -9,6 +9,7 @@ import (
 	// Internal
 	"github.com/salsita/salsaflow/app"
 	"github.com/salsita/salsaflow/config"
+	"github.com/salsita/salsaflow/errs"
 	"github.com/salsita/salsaflow/git"
 	"github.com/salsita/salsaflow/log"
 	"github.com/salsita/salsaflow/modules"
@@ -20,23 +21,26 @@ import (
 
 var Command = &gocli.Command{
 	UsageLine: `
-  start [-future_release=FUTURE]`,
-	Short: "start the release branch",
+  start [-next_trunk_version=VERSION]`,
+	Short: "start a new release",
 	Long: `
-  Start a new release by creating the release branch from the trunk branch.
-  More specifically, the steps are:
+  Start a new release by creating the release branch from the trunk branch
+  and making the relevant modifications in the issue tracker. More specifically,
+  the steps are:
 
-    1) Get the future release version string, either from the relevant flag
+    1) Get the next trunk version string, either from the relevant flag
        or read it from package.json on the trunk branch and auto-increment.
-    2) Ask the user to confirm the future release version string and the new release.
-    1) Create the release branch on top of the trunk branch.
+    2) Ask the user to confirm the next trunk version string and the new release.
+    3) Create the release branch on top of the trunk branch.
     4) Commit the new version string into the trunk branch so that it is
-       prepared for the future release.
-    5) Push everything.
+       prepared for the future release (the release after the one being started).
+	5) Start the release in the issue tracker.
+    6) Push everything.
 
-  So, the -future_release flag is actually not for the release that is
-  about to be started, but for the release after. The release that is
-  about to be started reads its version from package.json on that branch.
+  So, the -next_trunk_version flag is actually not affecting the release that is
+  about to be started, but the release after. The release that is about to be
+  started reads its version from the current trunk's package.json. This version
+  string is not modified during the execution of this command.
 	`,
 	Action: run,
 }
@@ -44,7 +48,7 @@ var Command = &gocli.Command{
 var flagFuture version.Version
 
 func init() {
-	Command.Flags.Var(&flagFuture, "future_release", "the future version string")
+	Command.Flags.Var(&flagFuture, "next_trunk_version", "the next trunk version string")
 }
 
 func run(cmd *gocli.Command, args []string) {
@@ -60,85 +64,77 @@ func run(cmd *gocli.Command, args []string) {
 	}
 }
 
-func runMain() (err error) {
-	var (
-		msg           string
-		stderr        *bytes.Buffer
-		currentBranch string
-	)
-	defer func() {
-		// Print error details.
-		if err != nil {
-			log.FailWithDetails(msg, stderr)
-		}
+func handleError(task string, err error, stderr *bytes.Buffer) error {
+	errs.NewError(task, stderr, err).Log(log.V(log.Info))
+	return err
+}
 
-		// Checkout the original branch.
-		if currentBranch == "" {
-			return
-		}
-		msg = "Checkout the original branch"
-		log.Run(msg)
-		out, ex := git.Checkout(currentBranch)
-		if ex != nil {
-			log.FailWithDetails(msg, out)
-			return
-		}
-	}()
+func runMain() (err error) {
+	// Fetch the remote repository.
+	msg := "Fetch the remote repository"
+	log.Run(msg)
+	stderr, err := git.UpdateRemotes(config.OriginName)
+	if err != nil {
+		return handleError(msg, err, stderr)
+	}
+
+	// Make sure that the trunk branch is up to date.
+	msg = "Make sure that the trunk branch is up to date"
+	log.Run(msg)
+	stderr, err = git.EnsureBranchSynchronized(config.TrunkBranch, config.OriginName)
+	if err != nil {
+		return handleError(msg, err, stderr)
+	}
+
+	// Make sure that the release branch does not exist.
+	msg = "Make sure that the release branch does not exist"
+	log.Run(msg)
+	stderr, err = git.EnsureBranchNotExists(config.ReleaseBranch, config.OriginName)
+	if err != nil {
+		return handleError(msg, err, stderr)
+	}
 
 	// Read the current trunk version string.
 	msg = "Read the current trunk version string"
 	trunkVersion, stderr, err := version.ReadFromBranch(config.TrunkBranch)
 	if err != nil {
-		return
+		return handleError(msg, err, stderr)
 	}
 
-	// Get the issue tracker instance and ensure that a new release can be started.
-	msg = "Fetch stories from the issue tracker"
+	// Fetch the stories from the issue tracker.
+	msg = "Fetch the stories from the issue tracker"
 	log.Run(msg)
 	release, err := modules.GetIssueTracker().NextRelease(trunkVersion)
 	if err != nil {
-		return
+		return handleError(msg, err, nil)
 	}
+
+	// Prompt the user to confirm the release.
+	msg = ""
 	ok, err := release.PromptUserToConfirmStart()
 	if err != nil {
-		return
+		return errs.LogAndReturn(err)
 	}
 	if !ok {
 		fmt.Println("\nYour wish is my command, exiting now...")
-		return
+		return nil
 	}
+	fmt.Println()
 
 	// Remember the current branch.
 	msg = "Remember the current branch"
-	log.Run(msg)
-	currentBranch, stderr, err = git.CurrentBranch()
+	currentBranch, stderr, err := git.CurrentBranch()
 	if err != nil {
-		return
+		return handleError(msg, err, stderr)
 	}
-
-	// Fetch the remote repository.
-	msg = "Fetch the remote repository"
-	log.Run(msg)
-	stderr, err = git.UpdateRemotes(config.OriginName)
-	if err != nil {
-		return
-	}
-
-	// Make sure that release does not exist.
-	msg = "Make sure that the release branch does not exist"
-	log.Run(msg)
-	stderr, err = git.EnsureBranchNotExists(config.ReleaseBranch, config.OriginName)
-	if err != nil {
-		return
-	}
-
-	// Make sure that trunk is up to date.
-	msg = "Make sure that the trunk branch is up to date"
-	log.Run(msg)
-	stderr, err = git.EnsureBranchSynchronized(config.TrunkBranch, config.OriginName)
-	if err != nil {
-		return
-	}
+	defer func() {
+		// Checkout the original branch on exit.
+		msg := "Checkout the original branch"
+		log.Run(msg)
+		if stderr, err := git.Checkout(currentBranch); err != nil {
+			handleError(msg, err, stderr)
+		}
+	}()
 
 	// Get the next trunk version (the future release version).
 	var nextTrunkVersion *version.Version
@@ -153,14 +149,14 @@ func runMain() (err error) {
 	log.Run(msg)
 	stderr, err = git.Branch(config.ReleaseBranch, config.TrunkBranch)
 	if err != nil {
-		return
+		return handleError(msg, err, stderr)
 	}
 	defer func(taskMsg string) {
 		if err != nil {
+			// On error, delete the newly created release branch.
 			log.Rollback(taskMsg)
-			out, ex := git.Branch("-d", config.ReleaseBranch)
-			if ex != nil {
-				log.FailWithDetails(taskMsg, out)
+			if stderr, err := git.Branch("-d", config.ReleaseBranch); err != nil {
+				handleError("Delete the release branch", err, stderr)
 			}
 		}
 	}(msg)
@@ -170,18 +166,18 @@ func runMain() (err error) {
 	log.Run(msg)
 	origTrunk, stderr, err := git.Hexsha("refs/heads/" + config.TrunkBranch)
 	if err != nil {
-		return
+		return handleError(msg, err, stderr)
 	}
 	stderr, err = nextTrunkVersion.CommitToBranch(config.TrunkBranch)
 	if err != nil {
-		return
+		return handleError(msg, err, stderr)
 	}
 	defer func(taskMsg string) {
 		if err != nil {
+			// On error, reset the trunk branch to point to the original position.
 			log.Rollback(taskMsg)
-			out, ex := git.ResetKeep(config.TrunkBranch, origTrunk)
-			if ex != nil {
-				log.FailWithDetails(taskMsg, out)
+			if stderr, err := git.ResetKeep(config.TrunkBranch, origTrunk); err != nil {
+				handleError("Reset the trunk branch to the original position", err, stderr)
 			}
 		}
 	}(msg)
@@ -190,11 +186,14 @@ func runMain() (err error) {
 	msg = ""
 	action, err := release.Start()
 	if err != nil {
-		return
+		return errs.LogAndReturn(err)
 	}
 	defer func() {
 		if err != nil {
-			action.Rollback()
+			// On error, cancel the release in the issue tracker.
+			if err := action.Rollback(); err != nil {
+				handleError("Cancel the release in the issue tracker", err, nil)
+			}
 		}
 	}()
 
@@ -206,7 +205,7 @@ func runMain() (err error) {
 		config.ReleaseBranch+":"+config.ReleaseBranch,
 		config.TrunkBranch+":"+config.TrunkBranch)
 	if err != nil {
-		return
+		return handleError(msg, err, stderr)
 	}
 
 	return nil
