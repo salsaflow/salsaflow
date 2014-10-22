5ef5a4ee19518a184e8a49ce37a19c03e9fb659d
Ondřej Kupka <ondra.cap@gmail.com>
2014-10-22 13:49:00 +0200

    circleci: Build and archive the pre-push hook
    Change-Id: adc3aaec22
    Story-Id: SF-46

--- circle_pack.bash
+++ circle_pack.bash
@@ -20,12 +20,13 @@ for os in linux windows darwin; do
 	base="salsaflow-$VERSION-${os_suffix}"
 	mkdir -p "$DST/$base"
 
-	cp \
-		"$SOURCES/salsaflow_${os_suffix}${exe_suffix}" \
-		"$DST/$base/salsaflow${exe_suffix}"
-	cp \
-		"$SOURCES/bin/hooks/salsaflow-commit-msg/salsaflow-commit-msg_${os_suffix}${exe_suffix}" \
-		"$DST/$base/salsaflow-commit-msg${exe_suffix}"
+	cp "$SOURCES/salsaflow_${os_suffix}${exe_suffix}" \
+	   "$DST/$base/salsaflow${exe_suffix}"
+
+	for hook in commit-msg pre-push; do
+		cp "$SOURCES/bin/hooks/salsaflow-${hook}/salsaflow-${hook}_${os_suffix}${exe_suffix}" \
+		   "$DST/$base/salsaflow-${hook}${exe_suffix}"
+	done
 
 	(cd "$DST" && zip -r "${base}.zip" "$base/" && cp "${base}.zip" "$CIRCLE_ARTIFACTS/")
 done
