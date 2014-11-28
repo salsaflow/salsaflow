4b49b503eeaa64d8e3975ee185049708035c4f01
Ondřej Kupka <ondra.cap@gmail.com>
2014-11-28 17:57:19 +0100

    shell: Add shortcut for exec.Command
    Change-Id: 5c444029a5
    Story-Id: SF-99

--- shell/run.go
+++ shell/run.go
@@ -10,11 +10,19 @@ func Run(name string, args ...string) (stdout, stderr *bytes.Buffer, err error) 
 	stderr = new(bytes.Buffer)
 
 	cmd := exec.Command(name, args...)
-
 	cmd.Stdout = stdout
 	cmd.Stderr = stderr
 
 	err = cmd.Run()
+	return
+}
 
+func Command(name string, args ...string) (cmd *exec.Cmd, stdout, stderr *bytes.Buffer) {
+	stdout = new(bytes.Buffer)
+	stderr = new(bytes.Buffer)
+
+	cmd = exec.Command(name, args...)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
 	return
 }
