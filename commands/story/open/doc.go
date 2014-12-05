33f8e032f814b080c64fd2d7ac62b2f8cb27ea24
Ondřej Kupka <ondra.cap@gmail.com>
2014-12-05 15:20:21 +0100

    story open: Add docs
    [ci skip]
    
    Change-Id: 5edebb573c
    Story-Id: SF-94

---
+++ commands/story/open/doc.go
@@ -0,0 +1,12 @@
+/*
+Open the given story in the issue tracker.
+
+  story open STORY_ID
+
+Description
+
+This command can be used to open the story specified by STORY_ID in the issue
+tracker. This usually means that the web browser is opened on the home page of
+the given story.
+*/
+package openCmd
