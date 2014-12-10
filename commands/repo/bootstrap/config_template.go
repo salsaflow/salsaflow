0d59b72b532c0f97e511f21c518abea2b1493878
Ondřej Kupka <ondra.cap@gmail.com>
2014-12-10 13:15:15 +0100

    repo bootstrap: Improve the config template
    Put a bit more explanation into the file, expecially for the Pivotal
    Tracker module that contains some optional keys.
    
    Change-Id: 8cccbb7b31
    Story-Id: SF-100

--- commands/repo/bootstrap/config_template.go
+++ commands/repo/bootstrap/config_template.go
@@ -5,9 +5,18 @@ import (
 	"text/template"
 )
 
-const LocalConfigTemplate = `issue_tracker: "{{.IssueTrackerKey}}"
+const LocalConfigTemplate = `#  Unless stated otherwise, the keys that are commented out
+#  are required, so please uncomment them and fill in the values.
+
+issue_tracker: "{{.IssueTrackerKey}}"
 {{ if eq .IssueTrackerKey "pivotal_tracker" }}pivotal_tracker:
 #  project_id: 123456
+#
+#  THE FOLLOWING SECTION IS OPTIONAL.
+#  The values visible there are the default choices.
+#  In case the defaults are fine, just delete the section,
+#  otherwise uncomment what you need to change.
+#
 #  labels:
 #    point_me: "point_me"
 #    no_review: "no review"
@@ -15,10 +24,11 @@ const LocalConfigTemplate = `issue_tracker: "{{.IssueTrackerKey}}"
 #    verified: "qa+"
 #    skip_check_labels:
 #      - "wontfix"
-#      - "dupe"{{ end }}
-{{ if eq .IssueTrackerKey "jira" }}jira:
+#      - "dupe"
+{{ else if eq .IssueTrackerKey "jira" }}jira:
 #  server_url: "https://example.com/jira"
-#  project_key: "EX"{{ end }}
+#  project_key: "EX"
+{{ end }}
 code_review_tool: "{{.CodeReviewToolKey}}"
 {{ if eq .CodeReviewToolKey "review_board" }}review_board:
 #  server_url: "https://review.example.com"{{ end }}
