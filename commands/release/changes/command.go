d40968c5cb1624b0baf05f889ae43c6b2d19779f
Ondřej Kupka <ondra.cap@gmail.com>
2014-12-12 10:54:20 +0100

    review stage: Add missing functionality
    Found a Not implemented panic there, so finished the implementation.
    
    Change-Id: a57c278026
    Story-Id: SF-84

--- commands/release/changes/command.go
+++ commands/release/changes/command.go
@@ -13,6 +13,7 @@ import (
 	"github.com/salsaflow/salsaflow/git"
 	"github.com/salsaflow/salsaflow/log"
 	"github.com/salsaflow/salsaflow/modules"
+	"github.com/salsaflow/salsaflow/releases"
 	"github.com/salsaflow/salsaflow/version"
 
 	// Other
@@ -128,7 +129,7 @@ func runMain() error {
 	groups = changes.SortStoryChanges(groups, stories)
 
 	if flagToCherryPick {
-		groups, err = groupsToCherryPick(groups)
+		groups, err = releases.StoryChangesToCherryPick(groups)
 		if err != nil {
 			return errs.NewError(task, err, nil)
 		}
@@ -144,54 +145,4 @@ func runMain() error {
 	}
 
 	return nil
-}
-
-func groupsToCherryPick(groups []*changes.StoryChangeGroup) ([]*changes.StoryChangeGroup, error) {
-	// Get the commits that are reachable from the release branch.
-	gitConfig, err := git.LoadConfig()
-	if err != nil {
-		return nil, err
-	}
-	releaseBranch := gitConfig.ReleaseBranchName()
-
-	reachableCommits, err := git.ShowCommitRange(releaseBranch)
-	if err != nil {
-		return nil, err
-	}
-
-	reachableCommitMap := make(map[string]struct{}, len(reachableCommits))
-	for _, commit := range reachableCommits {
-		reachableCommitMap[commit.SHA] = struct{}{}
-	}
-
-	// Get the changes that needs to be cherry-picked.
-	var toCherryPick []*changes.StoryChangeGroup
-
-	for _, group := range groups {
-		// Prepare a new StoryChangeGroup to hold missing changes.
-		storyGroup := &changes.StoryChangeGroup{
-			StoryId: group.StoryId,
-		}
-
-	ChangesLoop:
-		// Loop over the story changes and the commits associated with
-		// these changes. A change needs cherry-picking in case there are
-		// some commits left when we drop the commits reachable from
-		// the release branch.
-		for _, change := range group.Changes {
-			for _, commit := range change.Commits {
-				if _, ok := reachableCommitMap[commit.SHA]; ok {
-					continue ChangesLoop
-				}
-			}
-
-			storyGroup.Changes = append(storyGroup.Changes, change)
-		}
-
-		if len(storyGroup.Changes) != 0 {
-			toCherryPick = append(toCherryPick, storyGroup)
-		}
-	}
-
-	return toCherryPick, nil
 }
