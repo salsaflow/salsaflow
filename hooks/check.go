8dbfae5af17f58b58bc319dc59d5d8d16d3ddc92
Ondřej Kupka <ondra.cap@gmail.com>
2015-05-22 15:21:20 +0200

    hooks/check: Rename typ to hookType
    Change-Id: 469a3cfd27
    Story-Id: unassigned

--- hooks/check.go
+++ hooks/check.go
@@ -31,18 +31,18 @@ const (
 
 const hookPrefix = "salsaflow-"
 
-func getHookFileName(typ HookType) string {
+func getHookFileName(hookType HookType) string {
 	if runtime.GOOS == "windows" {
-		return hookPrefix + string(typ) + ".exe"
+		return hookPrefix + string(hookType) + ".exe"
 	} else {
-		return hookPrefix + string(typ)
+		return hookPrefix + string(hookType)
 	}
 }
 
 // Check whether SalsaFlow git hook is used. Prompts user to install our hook if it isn't.
 //
 // When the force argument is set to true, the hook is replaced when though the version matches.
-func CheckAndUpsert(typ HookType, force bool) error {
+func CheckAndUpsert(hookType HookType, force bool) error {
 	// Declade some variables so that we can use goto.
 	var confirmed bool
 
@@ -52,7 +52,7 @@ func CheckAndUpsert(typ HookType, force bool) error {
 		return err
 	}
 
-	hookDestPath := filepath.Join(repoRoot, ".git", "hooks", string(typ))
+	hookDestPath := filepath.Join(repoRoot, ".git", "hooks", string(hookType))
 
 	// Try to get the hook version.
 	stdout, _, _ := shell.Run(hookDestPath, "-"+versionFlag)
@@ -70,27 +70,27 @@ func CheckAndUpsert(typ HookType, force bool) error {
 	if err != nil {
 		return errs.NewError(task, err, nil)
 	}
-	hookExecutable := filepath.Join(binDir, getHookFileName(typ))
+	hookExecutable := filepath.Join(binDir, getHookFileName(hookType))
 
 	// Check whether there is a hook already present in the repository.
 	// If there is no hook or there is a SalsaFlow hook returning a different version string,
 	// we don't have to ask the user, we can just install the hook.
-	task = fmt.Sprintf("Check whether there is a git %v hook already installed", typ)
+	task = fmt.Sprintf("Check whether there is a git %v hook already installed", hookType)
 	if _, err := os.Stat(hookDestPath); err != nil {
 		if os.IsNotExist(err) {
-			return copyHook(typ, hookExecutable, hookDestPath)
+			return copyHook(hookType, hookExecutable, hookDestPath)
 		}
 		return errs.NewError(task, err, nil)
 	}
 	if installedVersion != nil || force {
-		return copyHook(typ, hookExecutable, hookDestPath)
+		return copyHook(hookType, hookExecutable, hookDestPath)
 	}
 
 	// Prompt the user to confirm the SalsaFlow git commit-task hook.
-	task = fmt.Sprintf("Prompt the user to confirm the %v hook", typ)
+	task = fmt.Sprintf("Prompt the user to confirm the %v hook", hookType)
 	confirmed, err = prompt.Confirm(`
-I need my own git ` + string(typ) + ` hook to be placed in the repository.
-Shall I create or replace your current ` + string(typ) + ` hook?`)
+I need my own git ` + string(hookType) + ` hook to be placed in the repository.
+Shall I create or replace your current ` + string(hookType) + ` hook?`)
 	fmt.Println()
 	if err != nil {
 		return errs.NewError(task, err, nil)
@@ -104,22 +104,22 @@ Please make sure the executable located at
 
   %v
 
-runs as your `+string(typ)+` hook and run me again!
+runs as your `+string(hookType)+` hook and run me again!
 
 `, hookExecutable)
-		return errs.NewError(task, fmt.Errorf("SalsaFlow git %v hook not detected", typ), nil)
+		return errs.NewError(task, fmt.Errorf("SalsaFlow git %v hook not detected", hookType), nil)
 	}
 
-	return copyHook(typ, hookExecutable, hookDestPath)
+	return copyHook(hookType, hookExecutable, hookDestPath)
 }
 
 // copyHook installs the SalsaFlow git hook by copying the hook executable
 // from the expected absolute path to the git config hook directory.
-func copyHook(typ HookType, hookExecutable, hookDestPath string) error {
-	task := fmt.Sprintf("Install the SalsaFlow git %v hook", typ)
+func copyHook(hookType HookType, hookExecutable, hookDestPath string) error {
+	task := fmt.Sprintf("Install the SalsaFlow git %v hook", hookType)
 	if err := CopyFile(hookExecutable, hookDestPath); err != nil {
 		return errs.NewError(task, err, nil)
 	}
-	log.Log(fmt.Sprintf("SalsaFlow git %v hook installed", typ))
+	log.Log(fmt.Sprintf("SalsaFlow git %v hook installed", hookType))
 	return nil
 }
