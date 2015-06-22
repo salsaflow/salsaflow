010ba4278b59340ced68b91a1f606d83399c3226
Ondřej Kupka <ondra.cap@gmail.com>
2015-06-22 14:38:33 +0200

    action: Add ActionChain struct
    ActionChain is a chain of actions. You can push actions there, then call
    Rollback and the actions will be rolled back in the FILO (stack) manner.
    So calling ActionChain.Rollback() is pretty much the same as registering
    multiple rollback actions using defer. The advantage of ActionChain is
    that you can bundle the actions together and then return the whole
    ActionChain, which itself implements action.Action interface.
    
    Change-Id: a21d205ffa

--- action/rollback.go
+++ action/rollback.go
@@ -6,34 +6,69 @@ import (
 	"github.com/salsaflow/salsaflow/log"
 )
 
-// RollbackOnError is supposed to be used together with defer:
+// RollbackOnError is equivalent to RollbackTaskOnError(err, "", action).
+func RollbackOnError(err *error, action Action) {
+	RollbackTaskOnError(err, "", action)
+}
+
+// RollbackTaskOnError wraps an ActionChain to perform a rollback on error.
+func RollbackTaskOnError(err *error, task string, action Action) {
+	chain := NewActionChain()
+	chain.PushTask(task, action)
+	chain.RollbackOnError(err)
+}
+
+type actionRecord struct {
+	task   string
+	action Action
+}
+
+type ActionChain struct {
+	actions []*actionRecord
+}
+
+func NewActionChain() *ActionChain {
+	return &ActionChain{}
+}
+
+func (chain *ActionChain) Push(action Action) {
+	chain.PushTask("", action)
+}
+
+func (chain *ActionChain) PushTask(task string, action Action) {
+	if action != nil {
+		chain.actions = append(chain.actions, &actionRecord{task, action})
+	}
+}
+
+func (chain *ActionChain) Rollback() error {
+	for i := range chain.actions {
+		act := chain.actions[len(chain.actions)-1-i]
+
+		// Inform the user what is happening.
+		if task := act.task; task != "" {
+			log.Rollback(task)
+		}
+
+		// Run the rollback function registered.
+		if err := act.action.Rollback(); err != nil {
+			errs.Log(err)
+		}
+	}
+	return nil
+}
+
+// RollbackOnError is supposed to be called using defer:
 //
-//     defer action.RollbackOnError(&err, action)
+//     defer chain.RollbackOnError(&err)
 //
 // Then it is visible why *error is being passed in. The args are bound
 // when this line is encountered, so it would not be set to a non-nil error
 // in case the error is set later. That is why we pass a pointer so that
 // err can be checked when the deferred function is actually called.
-func RollbackOnError(err *error, action Action) {
-	RollbackTaskOnError(err, "", action)
-}
-
-// RollbackTaskOnError does the same thing as RollbackOnError,
-// but it also informs the user that the given task is being reverted.
-func RollbackTaskOnError(err *error, task string, action Action) {
-	// Do nothing unless there is an error.
-	// In case action is nil, this is a noop.
-	if *err == nil || action == nil {
-		return
-	}
-
-	// Tell the user what is happening.
-	if task != "" {
-		log.Rollback(task)
-	}
-
-	// Call the rollback function.
-	if ex := action.Rollback(); ex != nil {
-		errs.Log(ex)
+func (chain *ActionChain) RollbackOnError(err *error) {
+	// Run the rollback function in case there is an error.
+	if *err != nil {
+		chain.Rollback()
 	}
 }
