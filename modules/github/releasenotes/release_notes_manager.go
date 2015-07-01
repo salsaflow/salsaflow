a12b1456d19ccb6695ab7fac20df537322d4291b
Ondřej Kupka <ondra.cap@gmail.com>
2015-07-01 07:51:26 +0200

    release deploy: Post release notes
    A release notes module can be configured now to post release notes when the
    release is being deployed. For now there is just a single module - 'github'.
    It creates a GitHub release and uploads the generated release notes there.
    
    Change-Id: 0120c6e8ad
    Story-Id: SF-177

---
+++ modules/github/releasenotes/release_notes_manager.go
@@ -0,0 +1,99 @@
+package github
+
+import (
+	// Stdlib
+	"bytes"
+	"fmt"
+
+	// Internal
+	"github.com/salsaflow/salsaflow/action"
+	"github.com/salsaflow/salsaflow/errs"
+	"github.com/salsaflow/salsaflow/git"
+	"github.com/salsaflow/salsaflow/github"
+	"github.com/salsaflow/salsaflow/log"
+	"github.com/salsaflow/salsaflow/modules/common"
+	"github.com/salsaflow/salsaflow/releases/notes"
+
+	// Vendor
+	gh "github.com/google/go-github/github"
+	"github.com/toqueteos/webbrowser"
+)
+
+const Id = "github"
+
+type releaseNotesManager struct{}
+
+func Factory() (common.ReleaseNotesManager, error) {
+	return &releaseNotesManager{}, nil
+}
+
+func (rnm *releaseNotesManager) PostReleaseNotes(
+	releaseNotes *common.ReleaseNotes,
+) (action.Action, error) {
+
+	// Get the GitHub owner and repository from the upstream URL.
+	owner, repo, err := git.ParseUpstreamURL()
+	if err != nil {
+		return nil, err
+	}
+
+	// Instantiate the API client.
+	config, err := github.LoadConfig()
+	if err != nil {
+		return nil, err
+	}
+	client := github.NewClient(config.ApiToken())
+
+	// Format the release notes.
+	task := "Format the release notes"
+	body := bytes.NewBufferString(`
+## Overview ##
+
+**PLEASE FILL IN THE RELEASE OVERVIEW**
+`)
+	encoder, err := notes.NewEncoder(notes.EncodingMarkdown, body)
+	if err != nil {
+		return nil, errs.NewError(task, err)
+	}
+	if err := encoder.Encode(releaseNotes, nil); err != nil {
+		return nil, errs.NewError(task, err)
+	}
+	bodyString := body.String()
+
+	// Create GitHub release for the given version.
+	tag := releaseNotes.Version.ReleaseTagString()
+	releaseTask := fmt.Sprintf("Create GitHub release for tag '%v'", tag)
+	log.Run(releaseTask)
+	release, _, err := client.Repositories.CreateRelease(owner, repo, &gh.RepositoryRelease{
+		TagName: gh.String(tag),
+		Name:    gh.String("Release " + releaseNotes.Version.BaseString()),
+		Body:    &bodyString,
+		Draft:   gh.Bool(true),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Delete the GitHub release on rollback.
+	rollback := func() error {
+		log.Rollback(releaseTask)
+		task := fmt.Sprintf("Delete GitHub release for tag '%v'", tag)
+		_, err := client.Repositories.DeleteRelease(owner, repo, *release.ID)
+		if err != nil {
+			return errs.NewError(task, err)
+		}
+		return nil
+	}
+
+	// Open the release in the browser so that the user can fill in the details.
+	task = "Open the release notes in the browser"
+	if err := webbrowser.Open(*release.HTMLURL); err != nil {
+		if ex := rollback(); ex != nil {
+			errs.Log(ex)
+		}
+		return nil, errs.NewError(task, err)
+	}
+
+	// Return the rollback function.
+	return action.ActionFunc(rollback), nil
+}
