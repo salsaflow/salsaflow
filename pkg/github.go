b25e9b97459903d4e7fdb4dfd8ab1709ab42d632
Ondřej Kupka <ondra.cap@gmail.com>
2015-09-16 15:05:27 +0200

    pkg: Align to the latest config management changes
    Change-Id: d322340c00
    Story-Id: SF-220

--- pkg/github.go
+++ pkg/github.go
@@ -1,15 +1,72 @@
 package pkg
 
 import (
-	"github.com/google/go-github/github"
-	ghutil "github.com/salsaflow/salsaflow/github"
+	// Internal
+	"github.com/salsaflow/salsaflow/config/loader"
+	"github.com/salsaflow/salsaflow/errs"
+	"github.com/salsaflow/salsaflow/github"
+	"github.com/salsaflow/salsaflow/prompt"
+
+	// Vendor
+	"github.com/bgentry/speakeasy"
+	gh "github.com/google/go-github/github"
 )
 
-func newGitHubClient() (*github.Client, error) {
-	config, err := ghutil.LoadConfig()
-	if err != nil {
-		return nil, err
+func newGitHubClient() (*gh.Client, error) {
+	task := "Instantiate a GitHub API client"
+
+	// Get the access token.
+	spec := newConfigSpec()
+	if err := loader.LoadConfig(spec); err != nil {
+		return nil, errs.NewError(task, err)
 	}
 
-	return ghutil.NewClient(config.ApiToken()), nil
+	// Return a new API client.
+	return github.NewClient(spec.global.GitHubToken), nil
+}
+
+// Configuration ===============================================================
+
+const ConfigKey = "salsaflow.core.updater"
+
+// Configuration spec ----------------------------------------------------------
+
+func newConfigSpec() *configSpec {
+	return &configSpec{}
+}
+
+type configSpec struct {
+	global *GlobalConfig
+}
+
+func (spec *configSpec) ConfigKey() string {
+	return ConfigKey
+}
+
+func (spec *configSpec) GlobalConfig() loader.ConfigContainer {
+	spec.global = &GlobalConfig{}
+	return spec.global
+}
+
+func (spec *configSpec) LocalConfig() loader.ConfigContainer {
+	return nil
+}
+
+// Global config ---------------------------------------------------------------
+
+type GlobalConfig struct {
+	GitHubToken string `json:"github_token"`
+}
+
+func (global *GlobalConfig) PromptUserForConfig() error {
+	token, err := speakeasy.Ask("Insert the GitHub token to be used for SalsaFlow updater: ")
+	if token == "" {
+		prompt.PanicCancel()
+	}
+	if err != nil {
+		return err
+	}
+
+	global.GitHubToken = token
+	return nil
 }
