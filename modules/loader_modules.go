83ec7c81cb5273f606e9db497c3428e089a56b32
Ondřej Kupka <ondra.cap@gmail.com>
2015-10-13 13:53:28 +0200

    Remove modules/code_review/reviewboard
    Drop the Review Board code review module. Better to use GitHub review issue for
    code review really...
    
    Change-Id: 27c38b8e22
    Story-Id: salsaflow/salsaflow#85

--- modules/loader_modules.go
+++ modules/loader_modules.go
@@ -4,7 +4,6 @@ import (
 	// Internal
 	"github.com/salsaflow/salsaflow/config/loader"
 	githubCodeReview "github.com/salsaflow/salsaflow/modules/code_review/github"
-	"github.com/salsaflow/salsaflow/modules/code_review/reviewboard"
 	githubIssueTracking "github.com/salsaflow/salsaflow/modules/issue_tracking/github"
 	"github.com/salsaflow/salsaflow/modules/issue_tracking/pivotaltracker"
 	githubReleaseNotes "github.com/salsaflow/salsaflow/modules/release_notes/github"
@@ -15,5 +14,4 @@ var registeredModules = []loader.Module{
 	githubIssueTracking.NewModule(),
 	githubReleaseNotes.NewModule(),
 	pivotaltracker.NewModule(),
-	reviewboard.NewModule(),
 }
