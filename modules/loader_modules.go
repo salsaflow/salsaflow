81c4de9d89d8ae73b36abb7292751bbe4688fbbd
Ondřej Kupka <ondra.cap@gmail.com>
2015-10-13 13:50:48 +0200

    Remove modules/issue_tracking/jira
    The module is not well suited for general use. Better to have no module than a
    module that is a piece of you know what.
    
    Change-Id: aa6fff2ce8
    Story-Id: salsaflow/salsaflow#85

--- modules/loader_modules.go
+++ modules/loader_modules.go
@@ -6,7 +6,6 @@ import (
 	githubCodeReview "github.com/salsaflow/salsaflow/modules/code_review/github"
 	"github.com/salsaflow/salsaflow/modules/code_review/reviewboard"
 	githubIssueTracking "github.com/salsaflow/salsaflow/modules/issue_tracking/github"
-	"github.com/salsaflow/salsaflow/modules/issue_tracking/jira"
 	"github.com/salsaflow/salsaflow/modules/issue_tracking/pivotaltracker"
 	githubReleaseNotes "github.com/salsaflow/salsaflow/modules/release_notes/github"
 )
@@ -15,7 +14,6 @@ var registeredModules = []loader.Module{
 	githubCodeReview.NewModule(),
 	githubIssueTracking.NewModule(),
 	githubReleaseNotes.NewModule(),
-	jira.NewModule(),
 	pivotaltracker.NewModule(),
 	reviewboard.NewModule(),
 }
