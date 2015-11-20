734440ddf367f5a76f64d5167e139901e78b7a4e
Ondřej Kupka <ondra.cap@gmail.com>
2015-11-20 10:56:57 +0100

    fileutil: Fix CopyFile docs
    Don't mention hardlinks since CopyFile is not creating them any more.
    
    Change-Id: 6d603ee04d
    Story-Id: salsaflow/salsaflow#125

--- fileutil/copy.go
+++ fileutil/copy.go
@@ -12,8 +12,7 @@ import (
 )
 
 // CopyFile copies a file from src to dst. If src and dst files exist, and are
-// the same, then return success. Otherise, attempt to create a hard link
-// between the two files. If that fail, copy the file contents from src to dst.
+// the same, then return success. Otherwise copy the file contents from src to dst.
 func CopyFile(src, dst string) (err error) {
 	sfi, err := os.Stat(src)
 	if err != nil {
